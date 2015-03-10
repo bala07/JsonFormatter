00829644a0b34fd931842c9b31ffd774adc562f7
Balasubramanian <sendmailtobalu@gmail.com>
2015-03-11 00:36:43 +0530

    Removing hardcoding of sample.json file path

--- src/test/java/com/tw/jsonFormatter/JsonTraverseTest.java
+++ src/test/java/com/tw/jsonFormatter/JsonTraverseTest.java
@@ -3,12 +3,17 @@ package com.tw.jsonFormatter;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.List;
 
 public class JsonTraverseTest {
     @Test
-    public void formatJson() throws IOException {
-        List<String> formattedJson = new JsonFormatter().format("/Users/balasubn/IdeaProjects/JsonFormatter/src/test/java/com/tw/jsonFormatter/sample.json");
+    public void formatJson() throws IOException, URISyntaxException {
+        URI uri = ClassLoader.getSystemResource("sample.json").toURI();
+
+        List<String> formattedJson = new JsonFormatter().format(uri.getPath());
 
         for(String jsonLine : formattedJson) {
             System.out.print(jsonLine);
